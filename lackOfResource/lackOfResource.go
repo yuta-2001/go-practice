367c15da34510a5b7e5d1f47feeaf52dd2b88755
yuta-2001 <yu0210ta.sug@gmail.com>
2024-09-04 01:25:05 +0900

    lack of resource

--- lackOfResource/lackOfResource.go
+++ lackOfResource/lackOfResource.go
@@ -3,7 +3,52 @@ package lackOfResource
 import (
 	"sync"
 	"time"
+    "fmt"
 )
 
 func main() {
+    var wg sync.WaitGroup
+    var sharedLock sync.Mutex
+    const runtime = 1*time.Second
+
+    greedyWorker := func() {
+        defer wg.Done()
+
+        var count int
+        for begin := time.Now(); time.Since(begin) <= runtime; {
+            sharedLock.Lock()
+            time.Sleep(3*time.Second)
+            sharedLock.Unlock()
+            count++
+        }
+        fmt.Printf("Greedy worker was able to execute %v work loops\n", count)
+    }
+
+    politeWorker := func() {
+        defer wg.Done()
+
+        var count int
+        for begin := time.Now(); time.Since(begin) <= runtime; {
+            sharedLock.Lock()
+            time.Sleep(1*time.Nanosecond)
+            sharedLock.Unlock()
+
+            sharedLock.Lock()
+            time.Sleep(1*time.Nanosecond)
+            sharedLock.Unlock()
+
+            sharedLock.Lock()
+            time.Sleep(1*time.Nanosecond)
+            sharedLock.Unlock()
+
+            count++
+        }
+
+        fmt.Printf("Polite worker was able to execute %v work loops.\n", count)
+    }
+
+    wg.Add(2)
+    go greedyWorker()
+    go politeWorker()
+    wg.Wait()
 }
