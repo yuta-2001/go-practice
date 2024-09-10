7232e241864a80eba3789e02d814fe1c0280a2f0
yuta-2001 <yu0210ta.sug@gmail.com>
2024-09-10 14:42:34 +0900

    orDone

---
+++ orDone/main.go
@@ -0,0 +1,31 @@
+package main
+
+
+func main() {
+	orDone := func(done, c <-chan interface{}) <-chan interface{} {
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-c:
+					if ok == false {
+						return
+					}
+					select {
+					case valStream <- v:
+					case <-done:
+					}
+				}
+			}
+		}()
+		return valStream
+	}
+	
+	done := make(chan interface{})
+	myChan := make(chan interface{})
+	
+	orDone(done, myChan)
+}
